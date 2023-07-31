f713d973c760ffec3c02c9d8a315a51a07e0b434
Elano <junioelano@gmail.com>
2023-07-30 21:17:05 -0300

    :tada: initial commit

---
+++ .vcscode/script.js
@@ -0,0 +1,14 @@
+function togglemode() {
+   const html = document.documentElement
+
+ //  if(html.classList.contains('light')) {
+   // html.classList.remove('light')
+  // } 
+   //else{
+   // html.classList.add('light')
+  // }
+// tudo esse codigo pode ser feito desa forma
+
+html.classList.toggle('light')
+
+}
